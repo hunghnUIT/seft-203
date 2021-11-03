d907d3b2ddb9f1c7d07da4a8f2afad39e6591143
KMS\hungnhoang <hungnhoang@kms-technology.com>
2021-11-03 23:12:48 +0700

    Test authorizer successfully with get user by email route but still do not know how it run

---
+++ authorizer.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const {
+  generateAuthResponse,
+} = require('./utils');
+const { getUserByEmail  } = require('./services/userService');
+
+module.exports.authorize = async (event, context, callback) => {
+  const token = event.authorizationToken.replace('Bearer ', '');
+  const methodArn = event.methodArn;
+
+  if (!token || !methodArn) return callback(null, 'Unauthorized');
+
+  // verifies token
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (decoded && decoded.email) {
+    const user = await getUserByEmail(decoded.email);
+    if (user)
+      return callback(null, generateAuthResponse(decoded.email, 'Allow', methodArn));
+  }
+  return callback(null, generateAuthResponse(decoded.email, 'Deny', methodArn));
+};
--- utils.js
+++ utils.js
@@ -107,3 +107,29 @@ exports.generateUserSk = (userEmail) => {
   sk = sk.replace('${userEmail}', userEmail);
   return sk;
 };
+
+exports.generateAuthResponse = (principalId, effect, methodArn) => {
+  const policyDocument = generatePolicyDocument(effect, methodArn);
+
+  return {
+    principalId,
+    policyDocument
+  };
+}
+
+const generatePolicyDocument = (effect, methodArn) => {
+  if (!effect || !methodArn) return null;
+
+  const policyDocument = {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: "execute-api:Invoke",
+        Effect: effect,
+        Resource: methodArn
+      }
+    ]
+  };
+
+  return policyDocument;
+}
